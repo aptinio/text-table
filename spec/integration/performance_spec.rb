ec75c47cf7f0f4401e8233439cf77b2631957ac9
Aaron Tinio <aptinio@gmail.com>
2013-04-16 01:28:22 +0000

    Use CPU time in performance test.

--- spec/integration/performance_spec.rb
+++ spec/integration/performance_spec.rb
@@ -3,8 +3,8 @@ require 'benchmark'
 
 describe Text::Table, 'performance' do
   it 'is linear relative to row count' do
-    base = time_to_render_num_of_rows 30
-    time = time_to_render_num_of_rows 300
+    base = time_to_render_num_of_rows  300
+    time = time_to_render_num_of_rows 3000
 
     time.should_not > base * 15
   end
@@ -12,8 +12,8 @@ describe Text::Table, 'performance' do
   def time_to_render_num_of_rows(num)
     GC.start
 
-    Benchmark.realtime do
+    Benchmark.measure {
       Text::Table.new(:rows => Array.new(num)).to_s
-    end
+    }.total
   end
 end
