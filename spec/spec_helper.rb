82e33aef4ad2a24165c5de576aacfbb814ad01f9
Mike Carlton <mike.carlton@illum.io>
2015-10-22 14:41:05 -0700

    fix spec helper to work with ruby 1.8.7

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,14 +13,14 @@ def decolor(table)
 end
 
 TERMINAL_COLOR = {
-  black:    0,
-  red:      1,
-  green:    2,
-  yellow:   3,
-  blue:     4,
-  magenta:  5,
-  cyan:     6,
-  white:    7
+  :black =>   0,
+  :red =>     1,
+  :green =>   2,
+  :yellow =>  3,
+  :blue =>    4,
+  :magenta => 5,
+  :cyan =>    6,
+  :white =>   7
 }
 
 def colorize(text, foreground = nil, background = nil)
