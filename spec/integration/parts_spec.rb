62d088a40d6ad4e5d9a23b4e1949effd8794341f
Antonio Terceiro <terceiro@softwarelivre.org>
2016-11-15 18:58:15 -0300

    Convert test suite to RSpec 3

--- spec/integration/parts_spec.rb
+++ spec/integration/parts_spec.rb
@@ -8,61 +8,61 @@ describe Text::Table do
   subject { table.to_s }
 
   describe 'rows' do
-    it { should == deindent(%q{
+    it { is_expected.to eq(deindent(%q{
       +-----+------+------+----+
       | aa  | bbb  | cccc | d  |
       | aaa | bbbb | c    | dd |
       +-----+------+------+----+
-    }) }
+    })) }
 
     context 'when nothing is passed into the contructor' do
       let(:rows) { nil }
 
       it 'is an empty array' do
-        table.rows.should == []
+        expect(table.rows).to eq([])
       end
     end
 
     context 'with unequal number of cells' do
-      pending
+      skip
     end
   end
 
   describe 'head' do
     let(:head) { @head }
 
-    it { should == deindent(%q{
+    it { is_expected.to eq(deindent(%q{
       +-----+------+------+------+
       |  a  |  bb  | ccc  | dddd |
       +-----+------+------+------+
       | aa  | bbb  | cccc | d    |
       | aaa | bbbb | c    | dd   |
       +-----+------+------+------+
-    }) }
+    })) }
   end
 
   describe 'foot' do
     let(:foot) { @foot }
 
-    it { should == deindent(%q{
+    it { is_expected.to eq(deindent(%q{
       +------+------+------+-----+
       | aa   | bbb  | cccc | d   |
       | aaa  | bbbb | c    | dd  |
       +------+------+------+-----+
       | aaaa | b    | cc   | ddd |
       +------+------+------+-----+
-    }) }
+    })) }
   end
 
   describe 'separators' do
     let(:rows) { @rows.insert(1, :separator) }
 
-    it { should == deindent(%q{
+    it { is_expected.to eq(deindent(%q{
       +-----+------+------+----+
       | aa  | bbb  | cccc | d  |
       +-----+------+------+----+
       | aaa | bbbb | c    | dd |
       +-----+------+------+----+
-    }) }
+    })) }
   end
 end
